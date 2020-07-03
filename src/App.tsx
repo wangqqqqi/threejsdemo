ada2931f5dfd28ca9d266158f10bb77c43aef3d1
wangqqqqi <1205767673@qq.com>
2020-07-03 09:32:05 +0800

    one

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Threedemo from './threedemo/threedemo'
+
+function App() {
+  return (
+    <div className="App">
+        <Threedemo></Threedemo>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/threedemo/threedemo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Threedemo extends React.Component{
+    render(){
+        return(
+            <div>hhhh</div>
+        )
+    }
+}
